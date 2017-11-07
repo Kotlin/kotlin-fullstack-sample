215331a7603fc46a16362b81cd01b03b2492a540
dn1345 <>
2017-11-07 16:33:08 +0100

    Replace gson dependency with ktor-gson

--- backend/src/org/jetbrains/demo/thinkter/Application.kt
+++ backend/src/org/jetbrains/demo/thinkter/Application.kt
@@ -1,18 +1,14 @@
 package org.jetbrains.demo.thinkter
 
-import com.google.gson.*
 import org.jetbrains.demo.thinkter.dao.*
 import io.ktor.application.*
-import io.ktor.content.*
 import io.ktor.features.*
+import io.ktor.gson.GsonConverter
 import io.ktor.http.*
 import io.ktor.locations.*
-import io.ktor.pipeline.PipelineContext
-import io.ktor.request.ApplicationReceiveRequest
 import io.ktor.response.respond
 import io.ktor.routing.*
 import io.ktor.sessions.*
-import org.jetbrains.demo.thinkter.model.RpcData
 
 data class Session(val userId: String)
 
@@ -48,18 +44,5 @@ fun Application.main() {
 
         login(storage, ::hash)
         register(storage, ::hash)
-    }
-}
-
-class GsonConverter(private val gson: Gson = Gson()) : ContentConverter {
-    override suspend fun convertForSend(context: PipelineContext<Any, ApplicationCall>, contentType: ContentType, value: Any): Any? {
-        return TextContent(gson.toJson(value), contentType.withCharset(context.call.suitableCharset()))
-    }
-
-    override suspend fun convertForReceive(context: PipelineContext<ApplicationReceiveRequest, ApplicationCall>): Any? {
-        val request = context.subject
-        val value = request.value as? IncomingContent ?: return null
-        val type = request.type
-        return gson.fromJson(value.readText(), type.javaObjectType)
     }
 }
